510d9c5fe723bc761101f4f4baca8d8a0cc3e5ab
maoruibin <gudong.name@gmail.com>
2016-11-05 12:38:58 +0800

    commit lost edit

---
+++ library/src/main/java/com/github/maoruibin/helper/IReviewControl.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 GuDong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.maoruibin.helper;
+
+import android.graphics.Canvas;
+
+/** apply a ability to have difference develop view
+ * Created by GuDong on 2016/10/13 10:45.
+ * Contact with gudong.name@gmail.com.
+ */
+public interface IReviewControl {
+    /**
+     * check now status is review mod or not default value is false.
+     * @return true if now is review mod else return false
+     */
+    boolean isReviewMod();
+
+    /**
+     * init paint info
+     */
+    void onInitPaint();
+
+    /**
+     * draw what do you want
+     * @param canvas
+     */
+    void onDraw(Canvas canvas);
+}
